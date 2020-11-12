435529acb07d2e5c340d3eb4e785034d1275f7dd
bumuthu <bumuthu.dilshan@gmail.com>
2020-11-12 21:25:42 +0530

    cam

--- camera_test.py
+++ camera_test.py
@@ -1,6 +1,6 @@
 import cv2
 pipeline = "rtspsrc location=\"rtsp://login:password@host:port/\" ! rtph264depay ! h264parse ! omxh264dec ! nvvidconv ! video/x-raw, format=(string)BGRx! videoconvert ! appsink"
-capture = cv2.VideoCaputure(pipeline, cv2.CAP_GSTREAMER)
+capture = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
 
 while capture.isOpened():
     res, frame = capture.read()

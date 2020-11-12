333818e74eaf91c87b533ca6d17bf6ed03293212
bumuthu <bumuthu.dilshan@gmail.com>
2020-11-12 21:35:23 +0530

    cam

--- camera_test.py
+++ camera_test.py
@@ -1,8 +1,10 @@
 import cv2
 pipeline = "rtspsrc location=\"rtsp://login:password@host:port/\" ! rtph264depay ! h264parse ! omxh264dec ! nvvidconv ! video/x-raw, format=(string)BGRx! videoconvert ! appsink"
 capture = cv2.VideoCapture(0, cv2.CAP_V4L)
-
+count = 0 
 while capture.isOpened():
+    print("hello", count)
+    count += 1
     res, frame = capture.read()
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1) & 0xFF

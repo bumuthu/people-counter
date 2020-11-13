c04f940a24a341a77f9782362835fa8dad770131
bumuthu <bumuthu.dilshan@gmail.com>
2020-11-13 11:21:18 +0530

    jet

--- jetson_det.py
+++ jetson_det.py
@@ -3,12 +3,12 @@ import jetson.utils
 
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 camera = jetson.utils.videoSource("csi://0")      # '/dev/video0' for V4L2
-display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
+display = jetson.utils.videoOutput("display://0")  # 'my_video.mp4' for file
 
 while True:
 	img = camera.Capture()
 	detections = net.Detect(img)
-    
+
     for detection in detections:
 		print(detection)
 

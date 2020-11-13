19deba4926b71bc68e2df0bc50d2be45ff8a84f7
bumuthu <bumuthu.dilshan@gmail.com>
2020-11-13 11:13:46 +0530

    jet

--- jetson_det.py
+++ jetson_det.py
@@ -5,8 +5,9 @@ net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 camera = jetson.utils.videoSource("csi://0")      # '/dev/video0' for V4L2
 display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
 
-while display.IsStreaming():
+while True:
 	img = camera.Capture()
 	detections = net.Detect(img)
+    print(detections)
 	display.Render(img)
 	display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))

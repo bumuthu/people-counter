c5e74b8d9adb6945409a1cfb0c0ac07a136fe4ed
bumuthu <bumuthu.dilshan@gmail.com>
2020-11-13 11:24:19 +0530

    jet

--- jetson_det.py
+++ jetson_det.py
@@ -6,9 +6,8 @@ camera = jetson.utils.videoSource("csi://0")      # '/dev/video0' for V4L2
 display = jetson.utils.videoOutput("display://0")  # 'my_video.mp4' for file
 
 while True:
-	img = camera.Capture()
-	detections = net.Detect(img)
-    
+    img = camera.Capture()
+    detections = net.Detect(img)
     for detection in detections:
 		print(detection)
 
